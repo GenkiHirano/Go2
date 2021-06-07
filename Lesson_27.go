0e8534965670584102e231922d4af764989c3520
GenkiHirano <genki_rad@ezweb.ne.jp>
2021-06-07 22:10:54 +0900

    Goの基礎_27

---
+++ Lesson_27.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a int
+	var b float64
+	var c string
+	var d bool
+	fmt.Printf("a: %d, b:%f, c:%s, d:%t\n", a, b, c, d)
+}
+
+// a: 0, b:0.000000, c:, d:false
