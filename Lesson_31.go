cfe0ffdcacbe2dc81b2b8f0469e3ee6bdff117d1
GenkiHirano <genki_rad@ezweb.ne.jp>
2021-06-11 17:15:30 +0900

    Goの基礎_31

---
+++ Lesson_31.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+// 複数の返り値を返す事ができる
+func swap(a int, b int) (int, int) {
+	return b, a
+}
+
+func main() {
+	fmt.Println(swap(5, 2))
+
+	// 関数もデータ型なので、変数に代入可能
+	// その際は関数名はいらない
+	f := func(a int, b int) (int, int) {
+		return b, a
+	}
+
+	fmt.Println(f(3, 8))
+
+	// 即時関数的な事も可能
+	func(msg string) {
+		fmt.Println(msg)
+	}("Fujimoto")
+}
