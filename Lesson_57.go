f485204e68bfa11aa38b6fdaf237efea436daa1c
GenkiHirano <genki_rad@ezweb.ne.jp>
2021-07-12 23:36:56 +0900

    Goの基礎_57

---
+++ Lesson_57.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+// メイン関数
+func main() {
+	signal := "blue"
+
+	switch signal {
+	case "red":
+		fmt.Println("Stop")
+	case "yellow":
+		fmt.Println("caution")
+	case "green", "blue":
+		fmt.Println("Go")
+	default:
+		fmt.Println("wrong")
+	}
+
+	// if-else的にも書く事が可能
+	score := 82
+	switch {
+	case score > 80:
+		fmt.Println("Great!")
+	default:
+		fmt.Println("Bad")
+	}
+}
