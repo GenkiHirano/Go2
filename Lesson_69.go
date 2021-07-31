375822c12b322852c768a68959f40ff8f4882791
GenkiHirano <genki86web@gmail.com>
2021-07-31 21:31:37 +0900

    Goの基礎_69

---
+++ Lesson_69.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+type greeter interface {
+	greet()
+}
+
+type japanese struct{}
+type american struct{}
+
+func (ja japanese) greet() {
+	fmt.Println("こんにちは！")
+}
+
+func (us american) greet() {
+	fmt.Println("Hello!")
+}
+
+func main() {
+	greeters := []greeter{japanese{}, american{}}
+
+	for _, greeter := range greeters {
+		greeter.greet()
+	}
+}
