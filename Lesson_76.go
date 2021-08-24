2057370d69c1552c55506e68e7106ca08d4ca7b7
GenkiHirano <genki86web@gmail.com>
2021-08-24 23:58:15 +0900

    Go基礎 Lesson_76.go

---
+++ Lesson_76.go
@@ -0,0 +1,30 @@
+type Person struct{}  //Person構造体
+type Person2 struct{} //Person2構造体
+
+//Person構造体のメソッドintro()
+func (p Person) intro() string {
+	return "Hello World"
+}
+
+//Person2構造体のメソッドintro()
+func (p Person2) intro() string {
+	return "Hello World"
+}
+
+//Person構造体のメソッドintro()を実行するメソッド
+func IntroForPerson(arg *Person) {
+	fmt.Println(arg.intro())
+}
+
+//Person2構造体のメソッドintro()を実行するメソッド
+func IntroForPerson2(arg *Person2) {
+	fmt.Println(arg.intro())
+}
+
+func main() {
+	bob := new(Person)
+	mike := new(Person2)
+
+	IntroForPerson(bob)   //=> Hello World
+	IntroForPerson2(mike) //=> Hello World
+}
