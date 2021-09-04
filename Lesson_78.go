b671b77eca08f64e7b2e9ca241519bb0b4203cf4
GenkiHirano <genki86web@gmail.com>
2021-09-04 21:29:18 +0900

    Go_基礎79

--- Lesson_78.go
+++ Lesson_78.go
@@ -3,12 +3,12 @@ package main
 import "fmt"
 
 func main() {
-    a := 5
+	a := 5
 
-    var pa *int // int型を格納する領域のアドレスを格納する準備
-    pa = &a     // &a = aのアドレス
+	var pa *int // int型を格納する領域のアドレスを格納する準備
+	pa = &a     // &a = aのアドレス
 
-    // paの領域にあるデータの値 = *pa
-    fmt.Println(pa)  // 0xc420018078
-    fmt.Println(*pa) // 5
+	// paの領域にあるデータの値 = *pa
+	fmt.Println(pa)  // 0xc420018078
+	fmt.Println(*pa) // 5
 }
