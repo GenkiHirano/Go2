8dbd39cb2c86c148b713313748b82f1812598194
GenkiHirano <genki86web@gmail.com>
2021-09-19 13:59:08 +0900

    Go_基礎82

---
+++ Lesson_82.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+type Person struct {
+	firstName string
+	age       int
+}
+
+func main() {
+	tim := Person{"Tim", 25}
+	person1 := &tim
+	(*person1).age = 25
+	person1.age = 53     //shortcutでp.Xと書くことも出来る
+	fmt.Println(person1) //=> {Tim 53}
+}
